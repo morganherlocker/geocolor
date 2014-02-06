3db7627d59ab8c0744e374fa5414b564c82ec507
Morgan Herlocker <morgan.herlocker@gmail.com>
2014-02-06 14:49:28 -0500

    removed color.js dependency

--- test/index.js
+++ test/index.js
@@ -12,16 +12,14 @@ describe('geocolor', function(){
     geo.features.should.be.ok
     geo.features[0].properties['marker-color'].should.be.ok
     fs.writeFileSync(__dirname+'/out/styled.geojson', JSON.stringify(geo, null, 2))
-    //console.log(geo)
   })
-  xit('should take a set of points and classify based on quantiles, then output geojson with color styles', function(){
+  it('should take a set of points and classify based on quantiles, then output geojson with color styles', function(){
     var geo = JSON.parse(fs.readFileSync(__dirname+'/in/points1.geojson'))
     geo.features[0].should.be.ok
 
-    geo = geocolor(geo, 'elevation', 'quantile', 5, 'red')
+    geo = geocolor(geo, 'elevation', 'quantile', 5, ['green', 'yellow', 'red'])
     geo.should.be.ok
     geo.features.should.be.ok
     geo.features[0].properties['marker-style'].should.be.ok
-    //console.log(geo)
   })
 })
