a6bef9479003892bd81a23354efcb31e0e480850
Morgan Herlocker <morgan.herlocker@gmail.com>
2014-02-06 15:56:29 -0500

    cities

--- test/index.js
+++ test/index.js
@@ -4,30 +4,30 @@ var should = require('should'),
 
 describe('geocolor', function(){
   it('should take a set of points and classify based on jenks, then output geojson with color styles', function(){
-    var geo = JSON.parse(fs.readFileSync(__dirname+'/in/points1.geojson'))
+    var geo = JSON.parse(fs.readFileSync(__dirname+'/in/cities.geojson'))
     geo.features[0].should.be.ok
 
-    geo = geocolor(geo, 'elevation', 'jenks', 5, ['green', 'yellow', 'red'])
+    geo = geocolor(geo, 'Population', 'jenks', 3, ['green', 'yellow', 'red'])
     geo.should.be.ok
     geo.features.should.be.ok
     geo.features[0].properties['marker-color'].should.be.ok
     fs.writeFileSync(__dirname+'/out/styled.geojson', JSON.stringify(geo, null, 2))
   })
   it('should take a set of points and classify based on quantiles, then output geojson with color styles', function(){
-    var geo = JSON.parse(fs.readFileSync(__dirname+'/in/points1.geojson'))
+    var geo = JSON.parse(fs.readFileSync(__dirname+'/in/cities.geojson'))
     geo.features[0].should.be.ok
 
-    geo = geocolor(geo, 'elevation', 'quantile', 7, ['green', 'yellow', 'red'])
+    geo = geocolor(geo, 'Population', 'quantile', 4, ['green', 'yellow', 'red'])
     geo.should.be.ok
     geo.features.should.be.ok
     geo.features[0].properties['marker-color'].should.be.ok
     fs.writeFileSync(__dirname+'/out/styled2.geojson', JSON.stringify(geo, null, 2))
   })
   it('should take a set of points and classify based on equal intervals, then output geojson with color styles', function(){
-    var geo = JSON.parse(fs.readFileSync(__dirname+'/in/points1.geojson'))
+    var geo = JSON.parse(fs.readFileSync(__dirname+'/in/cities.geojson'))
     geo.features[0].should.be.ok
 
-    geo = geocolor(geo, 'elevation', 'interval', 7, ['green', 'yellow', 'red'])
+    geo = geocolor(geo, 'Population', 'interval', 5, ['green', 'yellow', 'red'])
     geo.should.be.ok
     geo.features.should.be.ok
     geo.features[0].properties['marker-color'].should.be.ok
