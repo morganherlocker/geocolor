af5ca347ec884941ec73a2a286509a6be5a08677
Morgan Herlocker <morgan.herlocker@gmail.com>
2014-02-06 15:12:43 -0500

    equal intervals implemented

--- test/index.js
+++ test/index.js
@@ -23,4 +23,14 @@ describe('geocolor', function(){
     geo.features[0].properties['marker-color'].should.be.ok
     fs.writeFileSync(__dirname+'/out/styled2.geojson', JSON.stringify(geo, null, 2))
   })
+  it('should take a set of points and classify based on equal intervals, then output geojson with color styles', function(){
+    var geo = JSON.parse(fs.readFileSync(__dirname+'/in/points1.geojson'))
+    geo.features[0].should.be.ok
+
+    geo = geocolor(geo, 'elevation', 'interval', 7, ['green', 'yellow', 'red'])
+    geo.should.be.ok
+    geo.features.should.be.ok
+    geo.features[0].properties['marker-color'].should.be.ok
+    fs.writeFileSync(__dirname+'/out/styled3.geojson', JSON.stringify(geo, null, 2))
+  })
 })
