7612596df2fff4d1df65bf96d223f018e5a2068f
Morgan Herlocker <morgan.herlocker@gmail.com>
2014-02-07 15:35:08 -0500

    polygon fills

--- test/index.js
+++ test/index.js
@@ -33,4 +33,34 @@ describe('geocolor', function(){
     geo.features[0].properties['marker-color'].should.be.ok
     fs.writeFileSync(__dirname+'/out/styled3.geojson', JSON.stringify(geo, null, 2))
   })
+  xit('should take a set of points and classify based on custom breaks, then output geojson with color styles', function(){
+    var geo = JSON.parse(fs.readFileSync(__dirname+'/in/cities.geojson'))
+    geo.features[0].should.be.ok
+
+    geo = geocolor(geo, 'Population', [0,200000,300000,400000,500000,800000,1000000,1000000000], null, ['white', 'purple'])
+    geo.should.be.ok
+    geo.features.should.be.ok
+    geo.features[0].properties['marker-color'].should.be.ok
+    fs.writeFileSync(__dirname+'/out/styled4.geojson', JSON.stringify(geo, null, 2))
+  })
+  xit('should take a set of points and classify based on equal intervals, then output geojson with color styles', function(){
+    var geo = JSON.parse(fs.readFileSync(__dirname+'/in/cities.geojson'))
+    geo.features[0].should.be.ok
+
+    geo = geocolor(geo, 'Population', 'interval', 5, ['green', 'yellow', 'red'])
+    geo.should.be.ok
+    geo.features.should.be.ok
+    geo.features[0].properties['marker-color'].should.be.ok
+    fs.writeFileSync(__dirname+'/out/styled5.geojson', JSON.stringify(geo, null, 2))
+  })
+  it('should take a set of polygons and classify based on jenks, then output geojson with color styles', function(){
+    var geo = JSON.parse(fs.readFileSync(__dirname+'/in/sc.geojson'))
+    geo.features[0].should.be.ok
+
+    geo = geocolor(geo, 'poverty', 'quantile', 7, ['blue', 'red'])
+    geo.should.be.ok
+    geo.features.should.be.ok
+    geo.features[0].properties['fill'].should.be.ok
+    fs.writeFileSync(__dirname+'/out/styled7.geojson', JSON.stringify(geo, null, 2))
+  })
 })
