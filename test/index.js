4a5ececac037d0499b9568102212924134ac4296
Morgan Herlocker <morgan.herlocker@gmail.com>
2014-03-19 16:00:52 -0400

    all test

--- test/index.js
+++ test/index.js
@@ -109,4 +109,21 @@ describe('geocolor', function(){
     geo.features[0].properties['stroke'].should.equal('#ffffff')
     fs.writeFileSync(__dirname+'/out/styled11.geojson', JSON.stringify(geo, null, 2))
   })
+  it('should take a set of polygons and assign random colors with a custom opacity and stroke, then output geojson with color styles', function(){
+    var geo = JSON.parse(fs.readFileSync(__dirname+'/in/sc.geojson'))
+    geo.features[0].should.be.ok
+
+    var style = {
+      'stroke': 'white',
+      'fill-opacity': .4,
+      'fill': 'blue'
+    }
+    geo = geocolor.all(geo, style)
+    geo.should.be.ok
+    geo.features.should.be.ok
+    geo.features[0].properties['fill'].should.be.ok
+    geo.features[0].properties['fill-opacity'].should.equal(.4)
+    geo.features[0].properties['stroke'].should.equal('#ffffff')
+    fs.writeFileSync(__dirname+'/out/styled12.geojson', JSON.stringify(geo, null, 2))
+  })
 })
