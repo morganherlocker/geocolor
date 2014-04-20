1043873d92d8c65a984f260b234478b6a40aa382
Morgan Herlocker <morgan.herlocker@gmail.com>
2014-04-19 23:53:30 -0400

    basic control hide/show

--- js/site.js
+++ js/site.js
@@ -1,3 +1,36 @@
 $(function(){
-  //alert('blarg')
+  $('#classification').change(function(){
+    var classification = $('#classification').val()
+
+    if(classification === 'Jenks'){
+      $('#jenksControls').show()
+      $('#quantilesControls').hide()
+      $('#equalIntervalsControls').hide()
+      $('#randomControls').hide()
+    }
+    else if(classification === 'Quantiles'){
+      $('#quantilesControls').show()
+      $('#jenksControls').hide()
+      $('#equalIntervalsControls').hide()
+      $('#randomControls').hide()
+    }
+    else if(classification === 'Equal Interval'){
+      $('#equalIntervalsControls').show()
+      $('#jenksControls').hide()
+      $('#quantilesControls').hide()
+      $('#randomControls').hide()
+    }
+    else if(classification === 'Random'){
+      $('#randomControls').show()
+      $('#jenksControls').hide()
+      $('#quantilesControls').hide()
+      $('#equalIntervalsControls').hide()
+    }
+    else if(classification === 'All'){
+      $('#jenksControls').hide()
+      $('#quantilesControls').hide()
+      $('#equalIntervalsControls').hide()
+      $('#randomControls').hide()
+    }
+  })
 })
